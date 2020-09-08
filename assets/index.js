0c808003ad2fcced072400631b7ba9608f71f9dc
Ubuntu <ubuntu@ip-172-31-30-105.us-east-2.compute.internal>
2020-09-08 15:07:39 +0000

    add modal for collecting user input.

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,23 @@
 const myLibrary = [
-    {
+  {
     title: 'First-book',
     pages: 40,
     author: 'Rose',
     read: false,
-    },
-    {
+  },
+  {
     title: 'Second-book',
     pages: 60,
     author: 'Eli',
     read: false,
-    }
+  },
 ];
 
 function Book(title, pages, author, read) {
   this.title = title;
   this.pages = pages;
   this.author = author;
-  this.read = false;
+  this.read = read;
 }
 
 function addBookToLibrary(title, pages, author, read) {
@@ -27,11 +27,13 @@ function addBookToLibrary(title, pages, author, read) {
 
 const content = document.getElementById('content');
 let bookDiv = document.createElement('div');
+bookDiv.className = 'book';
 const title = document.createElement('h3');
 const page = document.createElement('p');
 const author = document.createElement('h4');
 const read = document.createElement('p');
-
+const newBookBtn = document.createElement('button');
+newBookBtn.innerHTML = 'Add new book';
 
 
 function displayBooks(bookArr) {
@@ -51,8 +53,17 @@ function displayBooks(bookArr) {
   });
 }
 
+function submitData(event) {
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const page = document.getElementById('page').value;
+  const read = document.getElementById('read').checked;
+  return addBookToLibrary(title, page, author, read);
+}
+
+const form = document.getElementById('form');
+
+form.onsubmit = submitData;
+
 displayBooks(myLibrary);
-
-
-
-
