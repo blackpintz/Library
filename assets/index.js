ee82f71499757f103bc27c9b2056dd5e909cc1ec
Ubuntu <ubuntu@ip-172-31-30-105.us-east-2.compute.internal>
2020-09-08 09:35:44 +0000

    add functions that loop through arrays and add books to library array.

--- assets/index.js
+++ assets/index.js
@@ -1,10 +1,40 @@
-const myLibrary = [''];
+const myLibrary = [{
+  title: 'First-book',
+  pages: 10,
+  author: 'Rose',
+  read: false,
+}, {
+  title: 'Second-book',
+  pages: 10,
+  author: 'Eli',
+  read: false,
+}];
 
-function Book() {
-  // the constructor...
+const content = document.getElementById('content');
+const bookDiv = document.createElement('div');
+const title = document.createElement('h3');
+const page = document.createElement('p');
+const author = document.createElement('h4');
+const read = document.createElement('p');
+
+bookDiv.innerHTML = 'Div book.';
+content.appendChild(bookDiv);
+
+function Book(title, pages, author, read) {
+  this.title = title;
+  this.pages = pages;
+  this.author = author;
+  this.read = false;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-  console.log(' ');
+function addBookToLibrary(title, pages, author, read) {
+  const book = new Book(title, pages, author, read);
+  return myLibrary.push(book);
+}
+
+function displayBooks(bookArr) {
+  bookArr.forEach((book) => {
+    title.innerHTML = book.title;
+    page.innerHTML = book.pages;
+  });
 }
