3470fb5bb31bb413cbf2e12b6264dda4b8432870
8Bts <mcmizze@yahoo.com>
2020-09-08 14:52:06 +0400

    Update displayBooks function

--- assets/index.js
+++ assets/index.js
@@ -1,24 +1,17 @@
-const myLibrary = [{
-  title: 'First-book',
-  pages: 10,
-  author: 'Rose',
-  read: false,
-}, {
-  title: 'Second-book',
-  pages: 10,
-  author: 'Eli',
-  read: false,
-}];
-
-const content = document.getElementById('content');
-const bookDiv = document.createElement('div');
-const title = document.createElement('h3');
-const page = document.createElement('p');
-const author = document.createElement('h4');
-const read = document.createElement('p');
-
-bookDiv.innerHTML = 'Div book.';
-content.appendChild(bookDiv);
+const myLibrary = [
+    {
+    title: 'First-book',
+    pages: 40,
+    author: 'Rose',
+    read: false,
+    },
+    {
+    title: 'Second-book',
+    pages: 60,
+    author: 'Eli',
+    read: false,
+    }
+];
 
 function Book(title, pages, author, read) {
   this.title = title;
@@ -32,9 +25,34 @@ function addBookToLibrary(title, pages, author, read) {
   return myLibrary.push(book);
 }
 
+const content = document.getElementById('content');
+let bookDiv = document.createElement('div');
+const title = document.createElement('h3');
+const page = document.createElement('p');
+const author = document.createElement('h4');
+const read = document.createElement('p');
+
+
+
 function displayBooks(bookArr) {
   bookArr.forEach((book) => {
+    bookDiv = bookDiv.cloneNode(false);
     title.innerHTML = book.title;
     page.innerHTML = book.pages;
+    author.innerHTML = book.author;
+    read.innerHTML = book.read;
+
+    bookDiv.appendChild(page.cloneNode(true));
+    bookDiv.appendChild(title.cloneNode(true));
+    bookDiv.appendChild(author.cloneNode(true));
+    bookDiv.appendChild(read.cloneNode(true));
+
+    content.appendChild(bookDiv);
   });
 }
+
+displayBooks(myLibrary);
+
+
+
+
